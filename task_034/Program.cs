e61620e27f449cc1ffa3e98b69cd5cb676d24ab2
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-08 19:57:38 +0300

    34

---
+++ task_034/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = new int[new Random().Next(3,21)];
+for(int i=0; i<array.Length;i++)
+{
+    array[i]=new Random().Next(100, 1000);
+}
+int count=0;
+for(int index=0;index<array.Length;index++)
+{
+if(array[index]%2==0)
+{
+    count++;
+}
+}
+Console.Clear();
+Console.Write("[{0}]",String.Join(", ",array));
+Console.WriteLine(" -> "+ count);
